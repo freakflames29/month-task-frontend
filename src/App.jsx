9a864cbb1f5dcb25be8867f7c8f8a2a26cf4acb6
Sourav Das <iamsouravdaswade420@gmail.com>
2025-01-25 22:25:35 +0530

    Added radixui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css"
+import { Flex, Text, Button, Container, Box } from "@radix-ui/themes";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Container size="4">
+        <Flex direction={"column"}>
+          <Text
+            size="9"
+            weight={"bold"}
+            mb={"2"}
+            mt={"9"}
+            color="red-10"
+            className={"Heading"}
+          >
+           Monthly Task Accomplisher
+          </Text>
+
+          <Box>
+            <Button
+              variant="classic"
+              className={"button"}
+              color={"red"}
+              loading={false}
+            >
+              Know me
+              {/* <DoubleArrowRightIcon/> */}
+            </Button>
+          </Box>
+        </Flex>
+      </Container>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
